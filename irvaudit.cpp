15a2cc212358ef5a95261733a7375602236e8efb
michelleblom <michelle.blom@gmail.com>
2019-09-18 17:10:13 +1000

    Updated licencing comments

--- irvaudit.cpp
+++ irvaudit.cpp
@@ -1,5 +1,20 @@
-// Code written by Michelle Blom, 2018
-// Please send any queries/bug notifications to michelle.blom@unimelb.edu.au
+/*
+    Copyright (C) 2018-2019  Michelle Blom
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 
 #include<iostream>
 #include<fstream>
