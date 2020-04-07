cfd1f98b666fc28e607c66e0d9453ffd92fb131f
michelleblom <michelle.blom@gmail.com>
2020-04-07 15:23:09 +1000

    Clean unnecessary files for this branch

--- run_tests.sh
+++
@@ -1,9 +0,0 @@
-./irvaudit -rep_ballots USIRV/Pierce_2008.raire -r 0.05  -agap 0.005 -simlog -json Pierce_2008.json
-
-./irvaudit -rep_ballots USIRV/Aspen_2009.raire -r 0.05  -agap 0.005 -simlog -json Aspen_2009.json
-
-./irvaudit -rep_ballots USIRV/Berkeley_2010.raire -r 0.05  -agap 0.005 -simlog -json Berkeley_2010.json
-
-./irvaudit -rep_ballots USIRV/Oakland_2010.raire -r 0.05 -agap 0.005 -simlog -json Oakland_2010.json
-
-./irvaudit -rep_ballots USIRV/SanFran_2007.raire -r 0.05  -agap 0.005 -simlog -json SanFran_2007.json
